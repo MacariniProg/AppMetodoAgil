eca88be55a3ce1ffaf68636037ecb4e842d6eda2
MacariniProg <macariniprog@gmail.com>
2024-04-04 23:38:42 -0300

    feat: sprint and project button with options

--- src/app/(app)/project/[id]/page.tsx
+++ src/app/(app)/project/[id]/page.tsx
@@ -5,6 +5,14 @@ import { useQuery } from "@tanstack/react-query";
 import { ProjectProps } from "@/src/types/project.types";
 import { Team } from "@/src/components/Project/Project/team";
 import { Sprint } from "@/src/components/Project/Project/Sprint";
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+} from "@/src/components/ui/menubar";
 
 export default function Project() {
   const { id } = useParams();
@@ -24,9 +32,23 @@ export default function Project() {
 
   return (
     <div className="flex-1 flex gap-4 flex-col overflow-hidden">
-      <h2 className="text-3xl font-bold tracking-tight">
-        Projeto {project.name}
-      </h2>
+      <div className="flex justify-between">
+        <h2 className="text-3xl font-bold tracking-tight">
+          Projeto {project.name}
+        </h2>
+
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>Ações</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>Nova sprint</MenubarItem>
+              <MenubarItem>Nova história</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Editar time</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+      </div>
       <div className="flex-1 gap-2 flex overflow-hidden">
         <div className="flex-[2] border-r pr-2  flex flex-col overflow-hidden">
           <Team users={project.users} />
--- src/components/Project/Project/Sprint/header.tsx
+++ src/components/Project/Project/Sprint/header.tsx
@@ -1,18 +1,38 @@
-import { CalendarIcon } from "lucide-react";
+import { CalendarIcon, GripVerticalIcon } from "lucide-react";
 import { format } from "date-fns";
 import { pt } from "date-fns/locale";
 import { Button } from "@/src/components/ui/button";
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+} from "@/src/components/ui/menubar";
 
 export function SprintHeader() {
   return (
     <div className="flex items-center justify-between">
       <h2 className="text-2xl font-semibold">Sprint 4</h2>
-      <div>
+      <div className="flex gap-2">
         <Button variant={"outline"} size="sm">
           <CalendarIcon className="mr-2 h-4 w-4" />
           {format(new Date(), "dd MMM, yyyy", { locale: pt })} -{" "}
           {format(new Date(), "dd MMM, yyyy", { locale: pt })}
         </Button>
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger className="!px-1">
+              <GripVerticalIcon size={16} />
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>Editar</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Finalizar</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
       </div>
     </div>
   );
